7d99948fa758e35487f82f596f82feb92def8b45
Eric Gomez <eric.gomez.sanchez@gmail.com>
2020-10-22 13:00:31 -0500

    MVVM Data Binding: integrando ViewModel a View

--- app/src/main/java/com/anncode/offersandcoupons/view/MainActivity.kt
+++ app/src/main/java/com/anncode/offersandcoupons/view/MainActivity.kt
@@ -2,15 +2,21 @@ package com.anncode.offersandcoupons.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import com.anncode.offersandcoupons.R
+import com.anncode.offersandcoupons.databinding.ActivityMainBinding
+import com.anncode.offersandcoupons.model.Coupon
+import com.anncode.offersandcoupons.viewmodel.CouponViewModel
 
 //El flujo comienza en *MainActivity y estara llamando a : CouponViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private var rvCoupons: RecyclerView? = null
+    private var couponViewModel: CouponViewModel? = null
 
     //Lo primero metodo que se ejecta es el onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,16 +24,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        //VIEW
-        //Declaramos nuestra Vista
-        rvCoupons = findViewById(R.id.rvCoupons)//UI
-        rvCoupons?.layoutManager =
-            LinearLayoutManager(this)
-        //<-VIEW
-
         //Estaremos ejecutando una llamada con CallCoupons
+        setupBindings(savedInstanceState)
         //Cuando la llamada se ejecuta se llena una list de cupones con getCoupons que trae la lista de Cupones
+    }
 
+    //pone en orbita el sistema de Bindeo que pusimos en el XML activiti main
+    fun setupBindings(savedInstanceState: Bundle?) {
+        var activityMainBinding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //Hacemos percistente todo lo que tenga nuestro couponViewModel con ViewModelProviders
+        couponViewModel = ViewModelProviders.of(this).get(CouponViewModel::class.java)
+
+        activityMainBinding.setModelBinding(couponViewModel)
+        setupListUpdate()
+    }
+    //Tendra toda la logica de levantar la lista de nuetro recyclerview
+    fun setupListUpdate() {
+        //CallCoupons
+        couponViewModel?.callCoupons()
+        //getCoupons - Lista de cupones
+        couponViewModel?.getCoupons()?.observe(this, Observer {
+            coupon: List<Coupon> ->
+            Log.w("COUPON", coupon.get(0).title)
+        })
     }
 
 }
