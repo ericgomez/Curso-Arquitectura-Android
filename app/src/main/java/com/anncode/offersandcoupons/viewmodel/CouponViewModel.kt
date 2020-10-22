88bfaeec7cd52e99658991d6caf54ca674d43e60
Eric Gomez <eric.gomez.sanchez@gmail.com>
2020-10-22 13:38:57 -0500

    MVVM Data Binding: RecyclerView Adapter

--- app/src/main/java/com/anncode/offersandcoupons/viewmodel/CouponViewModel.kt
+++ app/src/main/java/com/anncode/offersandcoupons/viewmodel/CouponViewModel.kt
@@ -2,14 +2,17 @@ package com.anncode.offersandcoupons.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.anncode.offersandcoupons.R
 import com.anncode.offersandcoupons.model.Coupon
 import com.anncode.offersandcoupons.model.CouponObservable
+import com.anncode.offersandcoupons.view.RecyclerCouponsAdapter
 
 //El flujo continua en *CouponViewModel y estara llamando a : CouponObservable
 //La clase CouponViewModel estara heredando de la clase ViewModel
 class CouponViewModel: ViewModel() {
     //Creamos una instacia a CouponObservable
     private var couponObservable: CouponObservable = CouponObservable()
+    private var recyclerCouponsAdapter: RecyclerCouponsAdapter? = null
 
     fun callCoupons(){
         couponObservable.callCoupons()
@@ -17,5 +20,15 @@ class CouponViewModel: ViewModel() {
 
     fun getCoupons(): MutableLiveData<List<Coupon>> {
         return couponObservable.getCoupons()
+    }
+
+    fun setCouponsInRecyclerAdapter(coupons: List<Coupon>) {
+        recyclerCouponsAdapter?.setCouponsList(coupons)
+        recyclerCouponsAdapter?.notifyDataSetChanged()//Para añadir un nuevo elemento
+    }
+
+    fun getRecyclerCouponsAdapter(): RecyclerCouponsAdapter? {
+        recyclerCouponsAdapter = RecyclerCouponsAdapter(this, R.layout.card_coupon)
+        return recyclerCouponsAdapter
     }
 }
